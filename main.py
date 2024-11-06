4c6dfc114d8eeda9ece830bad9edaf851dec0108
uncovercity <uncovercity@gmail.com>
2024-11-06 15:59:20 +0100

    fuzzywuzzy_prueba

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-#IMPORTS
+# IMPORTS
 from fastapi import FastAPI, Query, HTTPException, Request
 from typing import Optional
-from bistrohunter import obtener_restaurantes_por_ciudad, obtener_dia_semana, haversine, obtener_coordenadas #Llama a las funciones que hemos definido en el otro archivo de código
+from bistrohunter import obtener_restaurantes_por_ciudad, obtener_dia_semana  # Llama a las funciones necesarias
 import logging
 from datetime import datetime
 
 app = FastAPI()
 
-@app.get("/") #Define el mensaje por defecto de nuestra propia API 
+@app.get("/")  # Define el mensaje por defecto de nuestra propia API
 async def root():
     return {"message": "Bienvenido a la API de búsqueda de restaurantes"}
 
-@app.get("/api/getRestaurantsPrueba") #Dentro de nuestra propia API nosotros podemos llamar a diferentes funciones. Aquí llama a get_restaurantes
+@app.get("/api/getRestaurantsPrueba")  # Dentro de nuestra propia API podemos llamar a diferentes funciones. Aquí llama a obtener_restaurantes_por_ciudad
 async def get_restaurantes(
     request: Request,  
     city: str, 
@@ -19,7 +19,7 @@ async def get_restaurantes(
     price_range: Optional[str] = Query(None, description="El rango de precios deseado para el restaurante"),
     cocina: Optional[str] = Query(None, description="El tipo de cocina que prefiere el cliente"),
     diet: Optional[str] = Query(None, description="Dieta que necesita el cliente"),
-    dish: Optional[str] = Query(None, description="Plato por el que puede preguntar un cliente específicamente"),
+    dish: Optional[str] = Query(None, description="Plato específico por el que puede preguntar un cliente"),
     zona: Optional[str] = Query(None, description="Zona específica dentro de la ciudad"),
 ):
     try:
@@ -28,8 +28,17 @@ async def get_restaurantes(
             fecha = datetime.strptime(date, "%Y-%m-%d")
             dia_semana = obtener_dia_semana(fecha)
 
-        # Llamar a la función para obtener los restaurantes y la fórmula de filtro
-        restaurantes, filter_formula = obtener_restaurantes_por_ciudad(city, dia_semana, price_range, cocina, diet, dish, zona, sort_by_proximity=True)
+        # Llama a la función para obtener los restaurantes y la fórmula de filtro
+        restaurantes, filter_formula = obtener_restaurantes_por_ciudad(
+            city=city,
+            dia_semana=dia_semana,
+            price_range=price_range,
+            cocina=cocina,
+            diet=diet,
+            dish=dish,
+            zona=zona,
+            sort_by_proximity=True
+        )
         
         # Capturar la URL completa y los parámetros de la solicitud
         full_url = str(request.url)
@@ -75,8 +84,8 @@ async def get_restaurantes(
     except Exception as e:
         logging.error(f"Error al buscar restaurantes: {e}")
         raise HTTPException(status_code=500, detail="Error al buscar restaurantes")
-        
-@app.post("/procesar-variables") #Aquí llama a procesar_variables
+
+@app.post("/procesar-variables")  # Aquí llama a procesar_variables
 async def procesar_variables(request: Request):
     try:
         data = await request.json()
@@ -101,8 +110,8 @@ async def procesar_variables(request: Request):
             except ValueError:
                 raise HTTPException(status_code=400, detail="La fecha proporcionada no tiene el formato correcto (YYYY-MM-DD).")
 
-        # Llamar a la función para obtener los restaurantes
-        restaurantes = obtener_restaurantes_por_ciudad(
+        # Llama a la función para obtener los restaurantes
+        restaurantes, _ = obtener_restaurantes_por_ciudad(
             city=city,
             dia_semana=dia_semana,
             price_range=price_range,
@@ -122,11 +131,12 @@ async def procesar_variables(request: Request):
             return {
                 "restaurants": [
                     {
-                        "cid": restaurante['fields'].get('cid'),
+                        "cid": r['fields'].get('cid'),
                         "title": r['fields'].get('title', 'Sin título'),
                         "description": r['fields'].get('bh_message', 'Sin descripción'),
                         "price_range": r['fields'].get('price_range', 'No especificado'),
-                        "puntuacion_bistrohunter": restaurante['fields'].get('NBH2', 'N/A')
+                        "score": r['fields'].get('NBH2', 'N/A'),
+                        "url": r['fields'].get('url', 'No especificado')
                     } for r in restaurantes
                 ],
                 "variables": {
@@ -137,7 +147,7 @@ async def procesar_variables(request: Request):
                     "dish": dish,
                     "zone": zona
                 },
-                "api_call": api_call  # Devolver la llamada a la API
+                "api_call": api_call
             }
         else:
             return {
